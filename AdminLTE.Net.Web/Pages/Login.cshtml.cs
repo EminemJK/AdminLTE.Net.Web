2407b42fd1b9ee5cf97b954d3a4345b20a6376b9
huangxc <393089518@qq.com>
2018-11-22 18:02:31 +0800

    add

---
+++ AdminLTE.Net.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminLTE.Net.Web.Pages
+{
+    public class LoginedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
